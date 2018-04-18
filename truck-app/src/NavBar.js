51e6db96395aca1d3ca92c732ee69e370e63fae6
Alex Bush <alexabush7@gmail.com>
2018-04-18 15:55:48 -0700

    made some changes to styling of nav bar

--- truck-app/src/NavBar.js
+++ truck-app/src/NavBar.js
@@ -4,23 +4,59 @@ import { Route, Link } from 'react-router-dom';
 class NavBar extends Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(e) {
-    this.props.togglePage();
-  }
+  // handleClick(e) {
+  //   this.props.togglePage();
+  // }
 
   render() {
     return (
-      <div>
-        <p>Nav Bar</p>
+      // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      //   <a className="navbar-brand" href="#">
+      //     <i className="fas fa-truck-moving" />
+      //     <span className="ml-1">truckU</span>
+      //   </a>
+      //   <button
+      //     className="navbar-toggler"
+      //     type="button"
+      //     data-toggle="collapse"
+      //     data-target="#navbarSupportedContent"
+      //     aria-controls="navbarSupportedContent"
+      //     aria-expanded="false"
+      //     aria-label="Toggle navigation"
+      //   >
+      //     <span className="navbar-toggler-icon" />
+      //   </button>
+      //   <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      //     <ul className="navbar-nav ml-auto">
+      //       <li className="nav-item">
+      //         <Link to="/new" className="nav-link">
+      //           add new truck
+      //         </Link>
+      //       </li>
+      //       <li className="nav-item">
+      //         <Link to="/" className="nav-link">
+      //           all trucks
+      //         </Link>
+      //       </li>
+      //     </ul>
+      //   </div>
+      // </nav>
+
+      <nav className="navbar bg-primary navbar-dark">
+        {/* <p>Nav Bar</p> */}
         {/* {this.props.isShow ? ( */}
-        <Link to="/new">Add New Truck!</Link>
+        <p className="text-white">
+          <Link to="/new">Add New Truck!</Link>
+        </p>
         {/* ) : ( */}
-        <Link to="/">See all Trucks!</Link>
+        <p className="text-white">
+          <Link to="/">See all Trucks!</Link>
+        </p>
         {/* )} */}
-      </div>
+      </nav>
     );
   }
 }
